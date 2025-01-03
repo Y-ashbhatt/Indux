9da77e950df0f69554396636dcfcb2552fc4e598
Yash Bhatt <30y.bhatt@gmail.com>
2025-01-03 19:51:19 +0530

    ADD: hostel

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,18 @@ import DirectionClock from "./layouts/SearchSiderBar/_components/DirectionClock"
 import Home from "./pages/home/Home";
 import SimilarPlot from "./pages/plotView/_components/SimliarPlot";
 import SellerProfile from "./pages/sellerProfile/SellerProfile";
+import ViewHostel from "./pages/viewHostel/viewHostel";
 import SimilarProperties from "./pages/viewProperties/_components/SimilarProperties";
 
 function App() {
   return (
     <main>
-      <Home />
+      {/* <Home />  */}
       {/* <SellerProfile /> */}
       {/* <DirectionClock /> */}
       {/* <SimilarProperties /> */}
       {/* <SimilarPlot /> */}
+      <ViewHostel />
     </main>
   );
 }
