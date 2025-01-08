0cab8475b4cf07d2dca10ca3de0851dd9f1ee1fa
Yash Bhatt <30y.bhatt@gmail.com>
2025-01-08 19:08:47 +0530

    Fix: route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,6 @@ import SellerProfile from "./pages/sellerProfile/SellerProfile";
 import ViewHostel from "./pages/viewHostel/viewHostel";
 import SimilarProperties from "./pages/viewProperties/_components/SimilarProperties";
 import Footer from "./layouts/Footer";
-import BuyProperty from "./pages/BuyProperty/BuyProperty";
 import RentProperty from "./pages/RentProperty/RentProperty";
 import ListProperty from "./pages/ListProperty/ListProperty";
 import PageNotFound from "./layouts/PageNotFound";
@@ -16,11 +15,11 @@ import BuyProperties from "./pages/buyProperties/BuyProperties";
 function App() {
   return (
     <main className=" w-full block mx-auto overflow-hidden">
-      <Header/>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/seller/profile" element={<SellerProfile />} />
-        <Route path="/buy-properties" element={<BuyProperty />} />
+        <Route path="/buy-properties" element={<BuyProperties />} />
         <Route path="/rent-properties" element={<RentProperty />} />
         <Route path="/list-properties" element={<ListProperty />} />
         <Route path="/similar-plot" element={<SimilarPlot />} />
